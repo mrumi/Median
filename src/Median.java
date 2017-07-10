a7918f96363fb4a04e0c0dae39baa310702355e6
Lubaba <lubaba070@gmail.com>
2017-07-10 15:36:45 -0400

    last commit

--- src/Median.java
+++ src/Median.java
@@ -1,35 +1,35 @@
 
 public class Median {
 	
-	public static double findMedianSortedArrays(int[] A,int [] B)
+	public static double MedianSortedArrays(int[] A,int [] B)
 	{
 		int m=A.length,n=B.length;
 		int l=(m+n+1)/2;
 		int r = (m+n+2)/2;
-		return (getkth(A,0,B,0,l)+getkth(A,0,B,0,r))/2.0;
+		return (kth_num(A,0,B,0,l)+kth_num(A,0,B,0,r))/2.0;
 	}
 	
-	public static double getkth(int[]A, int aStart, int[] B, int bStart,int k)
+	public static double kth_num(int[]A, int as, int[] B, int bs,int k)
 	{
-		if(aStart >A.length-1) return B[bStart+k-1];
-		if(bStart>B.length-1) return A[aStart+k-1];
-		if(k ==1) return Math.min(A[aStart], B[bStart]);
+		if(as >A.length-1) return B[bs+k-1];
+		if(bs>B.length-1) return A[as+k-1];
+		if(k ==1) return Math.min(A[as], B[bs]);
 		
 		int aMid = Integer.MAX_VALUE,bMid = Integer.MAX_VALUE;
-		if(aStart+k/2-1<A.length) aMid = A[aStart+k/2 -1];
-		if(bStart+ k/2-1 <B.length) bMid = B[bStart+k/2-1];
+		if(as+k/2-1<A.length) aMid = A[as+k/2 -1];
+		if(bs+ k/2-1 <B.length) bMid = B[bs+k/2-1];
 		
 		if(aMid < bMid)
-			return getkth(A,aStart+k/2,B,bStart,k-k/2);
+			return kth_num(A,as+k/2,B,bs,k-k/2);
 		else
-			return getkth(A,aStart,B,bStart+k/2,k-k/2);
+			return kth_num(A,as,B,bs+k/2,k-k/2);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] array1 = {1,3};
 		int [] array2 = {2};
-		System.out.println(findMedianSortedArrays(array1,array2));
+		System.out.println(MedianSortedArrays(array1,array2));
 	}
 
 }
