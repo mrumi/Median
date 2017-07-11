52345f124ed14be90f49db50c000c69cdfd525c5
Lubaba <lubaba070@gmail.com>
2017-07-11 11:40:48 -0400

    regular expression added

--- src/Median.java
+++ src/Median.java
@@ -1,6 +1,8 @@
+import java.util.*;
 
 public class Median {
 	
+	/* median of two sorted arrays.*/
 	public static double MedianSortedArrays(int[] A,int [] B)
 	{
 		int m=A.length,n=B.length;
@@ -24,12 +26,68 @@ public class Median {
 		else
 			return kth_num(A,as,B,bs+k/2,k-k/2);
 	}
+	/*Regular expression matching*/
+	
+	public static boolean isMatch(String s, String p)
+	{
+		if(s==null|| p==null)
+			return false;
+		boolean [][] dp = new boolean[s.length()+1][p.length()+1];
+		dp[0][0] = true;
+		for(int i=0;i<p.length();i++)
+		{
+			if(p.charAt(i) == '*' && dp[0][i-1])
+			{
+				dp[0][i+1]=true;
+			}
+		}
+		for(int i=0;i<s.length();i++)
+		{
+			for(int j=0;j<p.length();j++)
+			{
+				if(p.charAt(j) == '.')
+				{
+					dp[i+1][j+1] =dp[i][j];
+				}
+				if(p.charAt(j)==s.charAt(i))
+				{
+					dp[i+1][j+1]=dp[i][j];
+				}
+				if(p.charAt(j)=='*')
+				{
+					if(p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '*')
+					{
+						dp[i+1][j+1]=dp[i+1][j-1];
+					}
+					else
+					{
+						dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+					}
+				}
+			}
+		}
+		for(int i=0;i<s.length();i++)
+		{
+			for(int j=0;j<p.length();j++)
+			{
+				System.out.print(dp[i][j] + " ");
+			}
+			System.out.println();
+		}
+		return dp[s.length()][p.length()];
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/* median of two sorted arrays.
 		int [] array1 = {1,3};
 		int [] array2 = {2};
-		System.out.println(MedianSortedArrays(array1,array2));
+		System.out.println(MedianSortedArrays(array1,array2));*/
+		
+		/*regular expression matching*/
+		String s1="aab";
+		String s2="c*a*b";
+		System.out.println(isMatch(s1,s2));
 	}
 
 }
